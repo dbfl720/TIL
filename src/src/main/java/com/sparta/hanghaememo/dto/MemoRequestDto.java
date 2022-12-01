a53af907fe6d10c5fc8de729ef386b941b925b3c
홍유리 <116433637+dbfl720@users.noreply.github.com>
2022-12-01 23:25:26 +0900

    개인과제 공부
    개인과제 공부 (주석처리로 설명)

---
+++ src/src/main/java/com/sparta/hanghaememo/dto/MemoRequestDto.java
@@ -0,0 +1,24 @@
+package com.sparta.hanghaememo.dto;
+//DTO(Data Transfer Object)란 계층간 데이터 교환을 위해 사용하는 객체(Java Beans)입니다.  여기서는 객체를 만듬!
+//getter & setter 메소드만 가진 클래스
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class MemoRequestDto {
+    private String username;   //객체
+    private String contents;
+    private String title;
+    private String password;
+
+
+}
+
+//    @NotBlank(message = "비밀번호 확인을 입력해주세요")
+//    private String checkpw;
+//
+//    }
---
+++ src/src/main/java/com/sparta/hanghaememo/repository/MemoRepository.java
@@ -0,0 +1,13 @@
+package com.sparta.hanghaememo.repository;
+
+
+
+import com.sparta.hanghaememo.entity.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//import java.lang.reflect.Member;
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();  //내림차순으로 모든 정보를 저장한다.
+}
